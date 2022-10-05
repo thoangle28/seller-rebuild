43b72583d6b62ddd672950a8a90c1e721db5816f
WIN-P654HAMHB3R\Administrator <voquang1406@gmail.com>
2022-10-05 16:57:22 +0700

    aspr-1_Set_up_project

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-import './app/Styles/App.scss'
+import './app/Sass/App.scss' 
 import store from './store'
 import { Provider } from 'react-redux'
 
