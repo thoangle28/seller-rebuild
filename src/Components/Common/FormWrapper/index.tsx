3f124fb9d3c1d2627f5e94c2abb62c032536f2d1
DESKTOP-IG1GMHR\lotus <thienthuann61@gmail.com>
2022-10-10 10:26:09 +0700

    complete task

---
+++ src/Components/Common/FormWrapper/index.tsx
@@ -0,0 +1,33 @@
+import './style.scss'
+
+import backLoginIcon from './../../../app/Images/icons/arrow-left.svg'
+
+type Props = {
+  children: JSX.Element
+  formTitle: string
+  backPagelogin?: boolean
+}
+
+const FormWrapper = (props: Props) => {
+  const {formTitle, children, backPagelogin} = props
+
+  return (
+    <div className='form-wrap'>
+      {backPagelogin ? (
+        <div className='back-login'>
+          <img
+            src={backLoginIcon}
+            alt='back-login'
+            className='back-login-icon'
+          />
+        </div>
+      ) : null}
+
+      <h3 className='form-title mb-3 text-center'>{formTitle}</h3>
+
+      {children}
+    </div>
+  )
+}
+
+export default FormWrapper
--- src/Components/Pages/Login/Example.tsx
+++ src/Components/Pages/Login/Example.tsx
@@ -1,15 +1,18 @@
-import { convertBase64 } from 'app/Utils'
+import {convertBase64} from 'app/Utils'
 import FullWidthLayout from 'Components/Layouts/FullWidthLayout'
-import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { add, getData } from './Redux/action'
-import MainLogo from './../../../app/Images/Addin.png'
+import {useState} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {add, getData} from './Redux/action'
+// import MainLogo from './../../../app/Images/Addin.png'
+import FormWrapper from 'Components/Common/FormWrapper'
 
 type Props = {}
 
 const Example = (props: Props) => {
   const dispatch = useDispatch()
-  const { number, data, isLoading } = useSelector((state: any) => state.loginReducer)
+  const {number, data, isLoading} = useSelector(
+    (state: any) => state.loginReducer
+  )
 
   const onClick = () => {
     dispatch(add(1))
@@ -19,52 +22,54 @@ const Example = (props: Props) => {
     getData(dispatch)
   }
 
-  const [baseImage, setBaseImage] = useState("");
+  const [baseImage, setBaseImage] = useState('')
 
   const [images, setImages] = useState<any[]>([])
 
   const uploadImage = async (e: any) => {
-    const file = e.target.files[0];
-    const base64: any = await convertBase64(file);
-    setBaseImage(base64);
-  };
+    const file = e.target.files[0]
+    const base64: any = await convertBase64(file)
+    setBaseImage(base64)
+  }
 
   const uploadMultipleImages = (e: any) => {
-    const { files } = e.target
+    const {files} = e.target
     for (const file of files) {
-      const reader = new FileReader();
+      const reader = new FileReader()
       reader.readAsDataURL(file)
       reader.onload = () => {
-        setImages(prev => [...prev, reader.result])
-      };
+        setImages((prev) => [...prev, reader.result])
+      }
       reader.onerror = () => {
         console.log(reader.error)
       }
     }
   }
-  return <FullWidthLayout>
-    <div className="wrapper w-50 mx-auto">
-      <div className="row">
-        <div className="col-sm-4">
+  return (
+    <FullWidthLayout>
+      {/* <div className='wrapper w-50 mx-auto'>
+        <div className='row'>
+          <div className="col-sm-4">
           <img src={MainLogo} alt="32" />
           <button onClick={onClick} className="btn btn-success" >Click Me</button>
           <p className="text-danger">{number}</p>
         </div>
-        <div className="p-10">
-          test
+          <div className='p-10'>test</div>
+          <input type='file' onChange={uploadImage} />
+          <img src={baseImage} alt='' />
+          <input type='file' onChange={uploadMultipleImages} multiple />
+          <div>
+            {images.map((img: any) => (
+              <img src={img} alt='' />
+            ))}
+          </div>
         </div>
-        <input type="file" onChange={uploadImage} />
-        <img src={baseImage} alt="" />
-        <input type="file" onChange={uploadMultipleImages} multiple />
-        <div>
-          {
-            images.map((img: any) => <img src={img} alt="" />)
-          }
-        </div>
-      </div>
-    </div>
-  </FullWidthLayout>
+      </div> */}
+      <FormWrapper formTitle='SIGN IN TO SELLER PORTAL' backPagelogin>
+        <h3></h3>
+      </FormWrapper>
+    </FullWidthLayout>
+  )
 }
-
 
 export default Example
