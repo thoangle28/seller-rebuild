682e7f625f22830645ba1041b8ff1e2fe2c8cbf4
DESKTOP-IG1GMHR\lotus <thienthuann61@gmail.com>
2022-10-11 10:21:19 +0700

    complete task create password validation

---
+++ src/Components/Pages/PasswordValidation/Schema/index.tsx
@@ -0,0 +1,21 @@
+import * as yup from 'yup'
+
+const pwdRule = /^(?=.*?[A-Z])(?=.*?[0-9]).{3,}$/
+const PasswordValidationSchema = yup.object().shape({
+  newPassword: yup
+    .string()
+    .min(3)
+    .matches(pwdRule, 'Invalid password')
+    .required('New password is required'),
+  passwordConfirm: yup
+    .string()
+    .oneOf([yup.ref('newPassword'), null], 'Passwords must match')
+    .required('Confirm password is required'),
+  password: yup
+    .string()
+    .min(3)
+    .matches(pwdRule, 'Invalid password')
+    .required('Password is required'),
+})
+
+export default PasswordValidationSchema
--- src/Components/Pages/PasswordValidation/index.tsx
+++ src/Components/Pages/PasswordValidation/index.tsx
@@ -1,5 +1,101 @@
-const index = () => {
-  return <div>index</div>
+import ButtonSubmitForm from 'Components/Common/ButtonSubmitForm'
+import FormWrapper from 'Components/Common/FormWrapper'
+import InputField from 'Components/Common/InputField'
+import FullWidthLayout from 'Components/Layouts/FullWidthLayout'
+import {useFormik} from 'formik'
+import {FC} from 'react'
+import PasswordValidationSchema from './Schema'
+
+import './style.scss'
+
+type Props = {}
+
+const ForgotPassword: FC = (props: Props) => {
+  const {
+    errors,
+    touched,
+    isSubmitting,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+    setFieldTouched,
+  } = useFormik({
+    initialValues: {
+      newPassword: '',
+      passwordConfirm: '',
+      password: '',
+    },
+    validationSchema: PasswordValidationSchema,
+    onSubmit: async (values) => {
+      console.log(values)
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+    },
+  })
+
+  const data = [
+    {
+      name: 'newPassword',
+      id: 'newPassword',
+      label: 'New Password',
+      type: 'password',
+      textError: errors.newPassword,
+      isError: errors.newPassword && touched.newPassword,
+    },
+    {
+      name: 'passwordConfirm',
+      id: 'passwordConfirm',
+      label: 'Password Confirm',
+      type: 'password',
+      textError: errors.passwordConfirm,
+      isError: errors.passwordConfirm && touched.passwordConfirm,
+    },
+    {
+      name: 'password',
+      id: 'password',
+      label: 'Password *',
+      type: 'password',
+      textError: errors.password,
+      isError: errors.password && touched.password,
+    },
+  ]
+
+  // Render list input
+  const inputFieldList = data.map((item) => {
+    return (
+      <InputField
+        key={item.id}
+        id={item.id}
+        name={item.name}
+        label={item.label}
+        type={item.type}
+        textError={item.textError}
+        onBlur={handleBlur}
+        onChange={(e) => {
+          handleChange(e)
+          setFieldTouched(item.name, false, false)
+        }}
+        isError={item.isError ? true : false}
+      />
+    )
+  })
+
+  return (
+    <FullWidthLayout>
+      <form className='form-pwd-validation' onSubmit={handleSubmit}>
+        <FormWrapper formTitle='FORGOT PASSWORD VALIDATION' backPageLogin>
+          <h4 className='form-sub-title text-center'>
+            <span>Enter your email to reset your password.</span>
+          </h4>
+
+          <div className='form__input-wrap'>
+            <div className='form__input-wrap-field'>{inputFieldList}</div>
+          </div>
+
+          <ButtonSubmitForm disabled={isSubmitting}>Submit</ButtonSubmitForm>
+        </FormWrapper>
+      </form>
+    </FullWidthLayout>
+  )
 }
 
-export default index
+export default ForgotPassword
