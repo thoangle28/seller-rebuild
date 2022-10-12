daccc44044db2a99b4d4d9e1fa69863299940f5c
WIN-P654HAMHB3R\Administrator <voquang1406@gmail.com>
2022-10-12 15:58:50 +0700

    Not found page

--- src/Components/Pages/NotFoundPage/index.tsx
+++ src/Components/Pages/NotFoundPage/index.tsx
@@ -1,11 +1,22 @@
 import DefaultLayout from 'Components/Layouts/DefaultLayout'
 import { FC } from 'react'
-type Props = {}
+import { useNavigate } from 'react-router-dom'
+import notfound from './../../../app/Images/error-404.png'
+import './style.scss'
 
-const NotFoundPage: FC = (props: Props) => {
+const NotFoundPage: FC = () => {
+    const navigate = useNavigate()
     return (
         <DefaultLayout>
-            <h1>Sorry Page Not Found</h1>
+            <div className="wrapper">
+                <div className='row m-0'>
+                    <div className="notfound-wrapper d-flex flex-column justify-content-center align-items-center col-lg-12 col-md-12 col-sm-12">
+                        <img className='mb-3' src={notfound} alt="not found" />
+                        <p>Page Not Found</p>
+                        <button onClick={()=> navigate('/dashboard')} className="btn btn-primary mt-4">Return To Dashboard</button>
+                    </div>
+                </div>
+            </div>
         </DefaultLayout>
     )
 }
