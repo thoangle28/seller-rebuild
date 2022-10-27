7028d8e6420a39bbde46020f9c5989a9ed6c4e77
ThienThuanVipPro <thienthuann61@gmail.com>
2022-10-27 14:34:23 +0700

    hide password validation url

--- src/Router/PortalRoute.tsx
+++ src/Router/PortalRoute.tsx
@@ -1,16 +1,17 @@
-import { lazy, Suspense } from 'react'
+import {lazy, Suspense} from 'react'
 import Login from '../Components/Pages/Login'
 import Signup from '../Components/Pages/SignUp'
 import ForgotPassword from 'Components/Pages/ForgotPassword'
 import NotFoundPage from '../Components/Pages/NotFoundPage'
-import PasswordValidation from '../Components/Pages/PasswordValidation'
 
-import { Routes, Route } from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 import ProtectedRoute from 'Components/Common/ProtectedRoute'
 
-const Dashboard = lazy(() => import("../Components/Pages/DashBoard"));
-const Profile = lazy(() => import("../Components/Pages/Profile"))
-const AccountSettings = lazy(() => import("../Components/Pages/AccountSettings"))
+const Dashboard = lazy(() => import('../Components/Pages/DashBoard'))
+const Profile = lazy(() => import('../Components/Pages/Profile'))
+const AccountSettings = lazy(
+  () => import('../Components/Pages/AccountSettings')
+)
 const ProductListing = lazy(() => import('../Components/Pages/ProductListing'))
 const Attributes = lazy(() => import('../Components/Pages/Attributes'))
 
@@ -31,8 +32,7 @@ const Router = (props: Props) => {
         <Route index element={<Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path='/password-validation' element={<PasswordValidation />} />
-      </Routes >
+      </Routes>
     </Suspense>
   )
 }
