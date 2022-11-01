1702a0f8b8be9ade55fb0a1e0963d9be31a735a8
ThienThuanVipPro <thienthuann61@gmail.com>
2022-11-01 15:15:50 +0700

    fix some bugs

--- src/Components/Common/Loading/index.tsx
+++ src/Components/Common/Loading/index.tsx
@@ -1,10 +1,11 @@
 import {FC} from 'react'
 import './style.scss'
+import loadingImg from './../../../app/Images/loading.png'
 
 const Loading: FC = () => {
   return (
     <div className='loading d-flex flex-column align-items-center'>
-      <div className='loading__icon'></div>
+      <img src={loadingImg} alt='loading' />
       <h6 className='loading__text m-0 mt-2 text-primary'>Loading...</h6>
     </div>
   )
