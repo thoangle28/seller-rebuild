37cdac3cd847722c43f6307d70dac649e7ebd9d6
TrinhQuang.Dev <voquang1406@gmail.com>
2022-11-02 08:57:54 +0700

    no message

--- src/Components/Common/ProtectedRoute/index.tsx
+++ src/Components/Common/ProtectedRoute/index.tsx
@@ -1,39 +1,30 @@
 import { iVerifyToken } from 'app/Models'
-import { logout, verifyToken } from 'Components/Pages/Login/Redux/action'
+import { verifyToken } from 'Components/Pages/Login/Redux/action'
 import { useEffect } from 'react'
 import { shallowEqual } from 'react-redux'
-import { Outlet, useNavigate, } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from './../../../app/Hooks/hooks'
 
 
 const ProtectedRoute = () => {
-    const navigate = useNavigate()
 
-    const { user, accessToken, expireDate } = useAppSelector(state => state.loginReducer, shallowEqual)
+    const { user, accessToken, isLogin } = useAppSelector(state => state.loginReducer, shallowEqual)
     const currentUserId: number = user ? user.ID : 0
 
     const dispatch = useAppDispatch()
 
+    const requestData = () => {
+        const payload: iVerifyToken = {
+            access_token: accessToken,
+            user_id: currentUserId.toString()
+        }
+        dispatch(verifyToken(payload))
+    }
     useEffect(() => {
-        const requestData = () => {
-            const payload: iVerifyToken = {
-                access_token: accessToken,
-                user_id: currentUserId.toString()
-            }
-            dispatch(verifyToken(payload))
-        }
-
-        const currentDate: number = Date.now();
-
-        if (accessToken && parseInt(expireDate) < currentDate && currentUserId) {
-            requestData() 
-        } else {
-            dispatch(logout())
-            navigate('/')
-        }
+        requestData()
     }, [])
 
-    return <><Outlet /></>
+    return <>{isLogin ? <Outlet /> : <Navigate to={'/'} replace />}</>
 }
 
 export default ProtectedRoute
