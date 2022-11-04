a20cf7c00a387e3391efc724c9e1cb6d7fc89b55
TrinhQuang.Dev <voquang1406@gmail.com>
2022-11-04 16:50:00 +0700

    no message

--- src/Router/PortalRoute.tsx
+++ src/Router/PortalRoute.tsx
@@ -1,27 +1,23 @@
-import {lazy, Suspense} from 'react'
+import { lazy, Suspense, FC } from 'react'
 import NotFoundPage from '../Components/Pages/NotFoundPage'
-
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import ProtectedRoute from 'Components/Common/ProtectedRoute'
-import FallBack from 'Components/Common/Fallback'
 
 const Dashboard = lazy(() => import('../Components/Pages/DashBoard'))
 const Profile = lazy(() => import('../Components/Pages/Profile'))
 const Login = lazy(() => import('../Components/Pages/Login'))
 const SignUp = lazy(() => import('../Components/Pages/SignUp'))
 const ForgotPassword = lazy(() => import('../Components/Pages/ForgotPassword'))
-const AccountSettings = lazy(
-  () => import('../Components/Pages/AccountSettings')
-)
+const AccountSettings = lazy(() => import('../Components/Pages/AccountSettings'))
 const ProductListing = lazy(() => import('../Components/Pages/ProductListing'))
 const Attributes = lazy(() => import('../Components/Pages/Attributes'))
 
-type Props = {}
 
-const Router = (props: Props) => {
+const Router: FC = () => {
   return (
-    <Suspense fallback={<FallBack />}>
+    <Suspense>
       <Routes>
+        {/* Declare Private Routes Here */}
         <Route element={<ProtectedRoute />}>
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/my-profile' element={<Profile />} />
@@ -30,6 +26,7 @@ const Router = (props: Props) => {
           <Route path='/Attributes' element={<Attributes />} />
           <Route path='/product-listing' element={<ProductListing />} />
         </Route>
+        {/* Declare Public Routes Here */}
         <Route index element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
