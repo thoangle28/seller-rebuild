c511b7a7d756ff0141f534639c197c44b0ead67f
TrinhQuang.Dev <voquang1406@gmail.com>
2022-11-07 15:44:52 +0700

    change axios config

--- src/APIs/AxiosConfig.ts
+++ src/APIs/AxiosConfig.ts
@@ -1,7 +1,20 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
 
+interface iLiveUrl {
+    [key: string]: string
+}
+
+const devBaseUrl: string = 'https://addin-sg.lotustest.net/wp-json/addin-seller/v1'
+
+const liveBaseUrl: iLiveUrl = {
+    'SG': 'https://aws.addin.sg/wp-json/addin-seller/v1'
+}
+
+const appMode = process.env.REACT_APP_MODE
+const location: string = process.env.REACT_APP_LOCATION || 'SG'
+
 const axiosConfig = axios.create({
-    baseURL: 'https://addin-sg.lotustest.net/wp-json/addin-seller/v1',
+    baseURL: appMode === 'DEV' ? devBaseUrl : liveBaseUrl[location],
     headers: {
         'Content-Type': 'application/json'
     }
@@ -12,8 +25,8 @@ axiosConfig.interceptors.request.use(function (config: AxiosRequestConfig) {
     return Promise.reject(error)
 })
 
-axiosConfig.interceptors.response.use(function (response: AxiosResponse) { 
-    return  response
+axiosConfig.interceptors.response.use(function (response: AxiosResponse) {
+    return response
 }, function (error) {
     return Promise.reject(error)
 })
